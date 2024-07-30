4609766c9d892d64ebdd8c771723c6be7a7aabea
Richard To <rto@Richards-MacBook-Pro.local>
2024-07-30 16:04:03 -0400

    individual books for the user

---
+++ src/components/UserBooks.jsx
@@ -0,0 +1,15 @@
+//return a div containing individual book data
+//look at market file from Unit 10
+import React, {useState, useEffect} from "react";
+
+const UserBook = ({ title, message, rating }) => {
+  return (
+    <div className='user-book'>
+      <p>Title: {title}</p>
+      <p>Message: {message}</p>
+      <p>Rating: {rating}</p>
+    </div>
+  )
+}
+
+export default UserBook;
