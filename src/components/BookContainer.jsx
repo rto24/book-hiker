9f57b071fc7a3f38d80a9d6dab4ec14318871f7b
Richard To <rto@Richards-MacBook-Pro.local>
2024-07-30 14:13:17 -0400

    react components

---
+++ src/components/BookContainer.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const BookContainer = () => {
+  return (
+    <div className="book-container">
+
+    </div>
+  )
+}
+
+export default BookContainer;
---
+++ src/components/MainContainer.jsx
@@ -0,0 +1,11 @@
+import React, { useState, useEffect } from "react";
+
+const MainContainer = () => {
+  return (
+    <div className="main-container">
+
+    </div>
+  )
+}
+
+export default MainContainer;
