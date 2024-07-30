2c6cc31421277eb4ddf99316cfd78b128e1f8d34
Richard To <rto@Richards-MacBook-Pro.local>
2024-07-30 15:25:42 -0400

    post method add

--- server/controller/userController.js
+++ server/controller/userController.js
@@ -16,4 +16,21 @@ userController.getBookNotes = (req, res, next) => {
     })
 }
 
+userController.postBookNotes = (req, res, next) => {
+  const { bookTitle, userMessage, rating } = req.body;
+  Book.create({ bookTitle, userMessage, rating })
+    .then(userBook => {
+      res.locals.userCollection = userBook;
+      // res.status(200).json(res.locals.userCollection);
+      return next();
+    })
+    .catch(err => {
+      return next({
+        log: `userController.postBookNotes: ERROR: ${err}`,
+        message: { err: `Error occurred at userController.postBookNotes` },
+        status: 500
+      })
+    })  
+}
+
 module.exports = userController;
