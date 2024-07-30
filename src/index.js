9003aa22a91241a1fb9b2392204ce27aa941fe1d
Richard To <rto@Richards-MacBook-Pro.local>
2024-07-30 17:49:03 -0400

    wrap app in browser router

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter } from "react-router-dom"
 import App from './App';
 
 // const node = document.getElementById('root');
 // const root = createRoot(node);
 // root.render(<App />);
-ReactDOM.render(<App />,document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"))
+root.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>
+)
+// ReactDOM.render(<App />,document.getElementById('root'));
