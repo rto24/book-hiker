57b624ccef92850a9df56b0aa2603ccc34d7eeec
Richard To <rto@Richards-MacBook-Pro.local>
2024-07-30 17:34:02 -0400

    nav

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const NavBar = () => {
+  return (
+    <nav className="nav">
+      <ul>
+        <li>
+          <a href="/">Your Books</a>  
+        </li>
+        <li>
+          <a href="/explore">Explore</a>  
+        </li>
+      </ul>
+    </nav>  
+  )
+}
+
+export default NavBar;
