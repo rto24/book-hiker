e1f614ea64f06640df54947e00eaea024032a076
Richard To <rto@Richards-MacBook-Pro.local>
2024-07-30 16:02:48 -0400

    imported BookCreator and BookDisplay

--- src/components/BookContainer.jsx
+++ src/components/BookContainer.jsx
@@ -1,9 +1,12 @@
 import React, { useState, useEffect } from "react";
-
+import BookCreator from "./BookCreator";
+import BookDisplay from "./BookDisplay";
+//children -> BookCreator and BookDisplay
 const BookContainer = () => {
   return (
     <div className="book-container">
-
+      <BookCreator />
+      <BookDisplay />
     </div>
   )
 }
