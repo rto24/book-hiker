6bbddee98db98529aa30f1d84d4fbde43ffd7859
Richard To <rto@Richards-MacBook-Pro.local>
2024-07-30 19:49:21 -0400

    home page

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,8 +4,10 @@ import Spline from '@splinetool/react-spline';
 
 const Home = () => {
   return (
-    <div>
-       <Spline
+    <div className="home-container">
+       <h1 className="home-header fade-in-top">BookHiker</h1>
+       <h4 className="home-subtext fade-in-bottom">A digital home for your book collections</h4>
+       <Spline className="home-spline"
         scene="https://prod.spline.design/fuDpuaZTwXb1lgFn/scene.splinecode" 
       />
     </div>
