c9e3f9e2e268087c522bcb46025ba51c28540ca2
Richard To <rto@Richards-MacBook-Pro.local>
2024-07-30 18:12:19 -0400

    home page

---
+++ src/components/Home.jsx
@@ -0,0 +1,15 @@
+// import { Route, Routes } from "react-router-dom"
+import React from "react";
+import Spline from '@splinetool/react-spline';
+
+const Home = () => {
+  return (
+    <div>
+       <Spline
+        scene="https://prod.spline.design/fuDpuaZTwXb1lgFn/scene.splinecode" 
+      />
+    </div>
+  )
+}
+
+export default Home;
