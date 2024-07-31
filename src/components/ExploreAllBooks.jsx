7d0dac312a7107f2990c95a4132458d1035c34de
Richard To <rto@Richards-MacBook-Pro.local>
2024-07-31 12:55:34 -0400

    container for exploring all books

---
+++ src/components/ExploreAllBooks.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ExploreAllBooks = () => {
+  return (
+    <div className="explore-all">
+      <h1>Go on a journey and find more books here</h1>
+    </div>
+  )
+}
+
+export default ExploreAllBooks;
--- src/components/ExploreContainer.jsx
+++ src/components/ExploreContainer.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import Spline from '@splinetool/react-spline';
+import ExploreAllBooks from "./ExploreAllBooks";
 
 const Explore = () => {
   return (
+    <>
+    <h1 className="explore-header">Explore all kinds of books</h1>
     <div className="explore-container">
-        
+      <ExploreAllBooks />
     </div>
+    </>
   )
 }
 
