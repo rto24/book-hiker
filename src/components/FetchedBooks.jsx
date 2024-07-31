cf935d8f8c932e856f1c97c9657f22f82e717737
Richard To <rto@Richards-MBP.cogeco.local>
2024-07-31 18:50:58 -0400

    added stars to each fetched book for the plan to read section

--- src/components/FetchedBooks.jsx
+++ src/components/FetchedBooks.jsx
@@ -3,6 +3,7 @@ import React, {useState} from "react";
 const FetchBooks = ({ title, bookImg, description }) => {
   return (
     <div className="fetched-book-container">
+      <button className="plan-to-read-btn">☆</button>
       <h1>{title}</h1>
       <img src={`${bookImg}`} alt="" />
       <p>{description}</p>
