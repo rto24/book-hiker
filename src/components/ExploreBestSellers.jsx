275ac6ff052948a0e6c03a2d588c2b39757cb4e5
Richard To <rto@Richards-MBP.cogeco.local>
2024-07-31 13:59:19 -0400

    best sellers section

---
+++ src/components/ExploreBestSellers.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ExploreBestSellers = () => {
+  return (
+    <div className="explore-bestsellers">
+      <h1 className="explore-bestsellers-header">Explore best selling books</h1>
+    </div>
+  )
+}
+
+export default ExploreBestSellers;
