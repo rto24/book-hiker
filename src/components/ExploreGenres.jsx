2f674716d476bfa2247d1de964a739be5358536c
Richard To <rto@Richards-MacBook-Pro.local>
2024-07-31 13:01:18 -0400

    new section for genres

---
+++ src/components/ExploreGenres.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const ExploreGenres = () => {
+  return (
+    <div className="explore-genres">
+      <h1 className="genre-header">Explore a new genre</h1>
+    </div>
+  )
+}
+
+export default ExploreGenres;
