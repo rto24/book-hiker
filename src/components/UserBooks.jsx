f743ca341d40e5cac40d64902172fe1283eb6bad
Richard To <rto@Richards-MacBook-Pro.local>
2024-07-30 20:04:27 -0400

    delete click handler for button

--- src/components/UserBooks.jsx
+++ src/components/UserBooks.jsx
@@ -2,9 +2,28 @@
 //look at market file from Unit 10
 import React, {useState, useEffect} from "react";
 
+const handleClick = async (event) => {
+  try {
+    const response = await fetch("/api/home", {
+      method: 'DELETE',
+      headers: {
+        'Content-Type': 'application/json',
+      }
+    })
+    if (response.ok) {
+      console.log('Book deleted');
+    } else {
+      console.log('Error uhoh', response.statusText);
+    }
+  } catch (error) {
+    console.log('Error:', error)
+  }
+};
+
 const UserBook = ({ title, message, rating }) => {
   return (
     <div className='user-book fade-in-right'>
+      <button onClick={handleClick} className="book-delete">X</button>
       <p><strong>Title:</strong> {title}</p>
       <p><strong>Message:</strong> {message}</p>
       <p><strong>Rating:</strong> {rating}</p>
