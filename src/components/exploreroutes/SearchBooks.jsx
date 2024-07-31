518df8e18aa366a59b970eab735ba07ceee3f200
Richard To <rto@Richards-MacBook-Pro.local>
2024-07-31 18:08:46 -0400

    added routes for explore components

---
+++ src/components/exploreroutes/SearchBooks.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ExploreBooksAPI from "../ExploreBooksAPI";
+
+const SearchBooks = () => {
+  return (
+    <div className="search-container">
+      <ExploreBooksAPI />
+    </div>
+  )
+}
+
+export default SearchBooks;
