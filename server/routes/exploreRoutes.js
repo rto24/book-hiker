d13e6670641b724778cc21ecc23c511772a1b810
Richard To <rto@Richards-MBP.cogeco.local>
2024-07-31 19:25:42 -0400

    get route setup

--- server/routes/exploreRoutes.js
+++ server/routes/exploreRoutes.js
@@ -1,4 +1,9 @@
 const express = require('express');
 const router = express.Router();
+const exploreController = require('../controller/exploreController');
+
+router.get('/', exploreController.getPlanToReadBooks, (req, res) => {
+  return res.status(200).json(res.locals.book);
+});
 
 module.exports = router;
