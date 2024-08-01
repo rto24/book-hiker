a15e73c8a2a31a1b7c93002d8dadf171036503fc
Richard To <rto@Richards-MBP.cogeco.local>
2024-07-31 20:39:47 -0400

    cleaned up comments

--- src/components/BookDisplay.jsx
+++ src/components/BookDisplay.jsx
@@ -1,12 +1,9 @@
-// fetch the user book list from database
-// iterate over this data to create displays using individual UserBook components 
 import React, { useState, useEffect } from "react";
-//need to import UserBooks still
 import UserBook from "./UserBooks";
 
 const BookDisplay = () => {
     const [bookData, setBookData] = useState([]);
-    const [loading, setLoading] = useState(true); // State to track loading status
+    const [loading, setLoading] = useState(true);
   
     useEffect(() => {
       const fetchData = async () => {
@@ -27,7 +24,7 @@ const BookDisplay = () => {
         } catch (error) {
           console.error('Error:', error);
         } finally {
-          setLoading(false); // Set loading to false after data is fetched
+          setLoading(false); 
         }
       };
   
@@ -35,7 +32,7 @@ const BookDisplay = () => {
     }, []);
   
     if (loading) return <p>Loading...</p>; 
-    if (bookData.length === 0) return <p>No data available.</p>;
+    if (bookData.length === 0) return <p>No data available</p>;
 
     return (
       <div className='book-display-container'>
