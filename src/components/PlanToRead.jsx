976907764de445a77d132f51c8ff2d0577e79e1c
Richard To <rto@Richards-MBP.cogeco.local>
2024-07-31 20:38:46 -0400

    mapped over data to display in plan to read

--- src/components/PlanToRead.jsx
+++ src/components/PlanToRead.jsx
@@ -1,9 +1,47 @@
-import React from "react";
+import React, {useState, useEffect } from "react";
+import FetchBooks from "./FetchedBooks";
 
 const PlanToReadList = () => {
+  const [ planToReadData, setPlanToReadData ] = useState([]);
+  const [ loading, setLoading ] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/explore", {
+          method: 'GET',
+          headers: {
+            'Content-Type': 'application/json',
+          }
+        });
+
+        if (response.ok) {
+          const data = await response.json();
+          setPlanToReadData(data);
+        } else {
+          console.error('Error fetching data:', response.statusText);
+        }
+      } catch (error) {
+        console.error('Error:', error);
+      } finally {
+        setLoading(false); // Set loading to false after data is fetched
+      }
+    };
+    fetchData();
+  }, []);
+
+  if (loading) return <p>Loading...</p>; 
+  if (planToReadData.length === 0) return <p>No data available</p>;
+
   return (
     <div className="plan-to-read-container">
+      {planToReadData.map((obj, index) => {
+        const keys = Object.keys(obj);
+        const title = obj[keys[1]];
+        const img = obj[keys[2]];
 
+        return <FetchBooks title={title} bookImg={img} key={index}/> //may need to make a new component, fetch books still has the button attached
+      })}
     </div>
   )
 }
