df33bbd8be660a517a7ff8defe6c234089bb7700
Richard To <rto@Richards-MacBook-Pro.local>
2024-08-01 15:21:44 -0400

    added class names to each element

--- src/components/Users.jsx
+++ src/components/Users.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const User = ({ id, firstName, lastName, userName }) => {
   return (
     <div className="user-info-container">
-      <h1>{userName}</h1>
-      <p>First Name: {firstName}</p>
-      <p>Last Name: {lastName}</p>
+      <h1 className="username">{userName}</h1>
+      <p className="user-first-name">First Name: {firstName}</p>
+      <p className="user-last-name">Last Name: {lastName}</p>
       <button className="connect-btn">Connect</button>
     </div>
   )
